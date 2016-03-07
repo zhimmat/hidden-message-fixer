e05abf620c4a69eb35d038c5a798491fd41b7916
zhimmat <zeyad.himmat@live.com>
2016-03-06 19:47:37 -0500

    fixed code

--- js/ main.js
+++ js/ main.js
@@ -1,6 +1,6 @@
-var hiddenMessage = '<p><span>\u2606</span>\x53\x75\x70\x65\x72\x73\x74\x61\x72\x21</p>;
+var hiddenMessage = '<p><span>\u2606</span>\x53\x75\x70\x65\x72\x73\x74\x61\x72\x21</p>';
 var i = 0;
 
 for (i = 0; i < 10 i++) {
   document.write(hiddenMessage);
-
+}
